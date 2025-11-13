8042cad0aa3336867e6fae7599c2b3a36d7c7005
Contre <lucascontre95@gmail.com>
2025-11-13 11:57:16 +0100

    fix: Config validation + folder creation on default

--- src/features/config/config.go
+++ src/features/config/config.go
@@ -2,8 +2,8 @@ package config
 
 // Config holds the application configuration.
 type Config struct {
-	LibraryPath  string      `yaml:"libraryPath"`
-	DownloadPath string      `yaml:"downloadPath"`
+	LibraryPath  string `yaml:"libraryPath" required:"true"`
+	DownloadPath string `yaml:"downloadPath" required:"true"`
 	Telegram     Telegram    `yaml:"telegram"`
 	Logger       Logger      `yaml:"logger"`
 	Downloaders  Downloaders `yaml:"downloaders"`
@@ -43,7 +43,7 @@ type Paths struct {
 
 // Database holds the configuration for the database
 type Database struct {
-	Path string `yaml:"path"`
+	Path string `yaml:"path" required:"true"`
 }
 
 // Server hold the configuration for the Fiber server Config
